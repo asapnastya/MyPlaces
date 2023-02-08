bb87edfbc35a4507a793006cda886ef8d1eb24bc
Анастасия Романова <>
2023-02-08 22:00:36 +0300

    feat: added Realm && new place screen && refactoring

---
+++ MyPlaces/NewPlaceViewController.swift
@@ -0,0 +1,169 @@
+//
+//  NewPlaceViewController.swift
+//  MyPlaces
+//
+//  Created by Анастасия Романова on 01.02.2023.
+//
+
+import UIKit
+import SwiftUI
+
+class NewPlaceViewController: UITableViewController {
+    
+    var currentPlace: Place?
+    var isImageChanged = false
+    
+    @IBOutlet weak var imageOfPlace: UIImageView!
+    @IBOutlet weak var saveButton: UIBarButtonItem!
+    @IBOutlet weak var placeName: UITextField!
+    @IBOutlet weak var placeLocation: UITextField!
+    @IBOutlet weak var placeType: UITextField!
+    
+    override func viewDidLoad() {
+        
+        super.viewDidLoad()
+        tableView.tableFooterView = UIView()
+        saveButton.isEnabled = false
+        placeName.addTarget(self, action: #selector(textFieldChanged), for: .editingChanged)
+        setupEditScreen()
+    }
+    
+    // MARK:   Table view delegate
+    override func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
+        
+        if indexPath.row == 0 {
+            
+            let cameraIcon = UIImage(named: "camera")
+            let photoLibraryIcon = UIImage(named: "photo")
+            
+            let actionSheet = UIAlertController(title: nil,
+                                                message: nil,
+                                                preferredStyle: .actionSheet)
+            
+            let camera = UIAlertAction(title: "Camera", style: .default) { _ in
+                self.chooseImagePicker(source: .camera)
+            }
+            
+            camera.setValue(cameraIcon, forKey: "image")
+            
+            let photoAlbum = UIAlertAction(title: "Photo Library", style: .default) { _ in
+                self.chooseImagePicker(source: .photoLibrary)
+            }
+            
+            photoAlbum.setValue(photoLibraryIcon, forKey: "image")
+            
+            let cancel = UIAlertAction(title: "Cancel", style: .cancel)
+            
+            actionSheet.addAction(camera)
+            actionSheet.addAction(photoAlbum)
+            actionSheet.addAction(cancel)
+            
+            present(actionSheet, animated: true)
+        } else {
+            view.endEditing(true)
+        }
+    }
+    
+    func savePlace() {
+                
+        var image: UIImage?
+        if isImageChanged {
+            image = imageOfPlace.image
+        } else {
+            image = UIImage(named: "imagePlaceholder")
+        }
+        
+        let imageData = image?.pngData()
+        
+        let newPlace = Place(name: placeName.text!,
+                             location: placeLocation.text,
+                             type: placeType.text,
+                             imageData: imageData)
+        
+        if currentPlace != nil {
+            try! realm.write {
+                currentPlace?.name = newPlace.name
+                currentPlace?.location = newPlace.type
+                currentPlace?.type = newPlace.type
+                currentPlace?.imageData = newPlace.imageData
+            }
+        } else {
+            StorageManager.saveObject(newPlace)
+        }
+    }
+    
+    private func setupEditScreen() {
+                
+        if currentPlace != nil {
+            setupNavigationBar()
+            isImageChanged = true
+ 
+            guard let data = currentPlace?.imageData,
+                  let image = UIImage(data: data) else { return }
+            
+            imageOfPlace.image = image
+            imageOfPlace.contentMode = .scaleToFill
+            placeName.text = currentPlace?.name
+            placeLocation.text = currentPlace?.location
+            placeType.text = currentPlace?.type
+        }
+    }
+    
+    private func setupNavigationBar() {
+        
+        if let topItem = navigationController?.navigationBar.topItem {
+            topItem.backBarButtonItem = UIBarButtonItem(title: "", style: .plain, target: nil, action: nil )
+        }
+        navigationItem.leftBarButtonItem = nil
+        title = currentPlace?.name
+        saveButton.isEnabled = true
+    }
+    
+        @IBAction func cancelAction(_ sender: Any) {
+            dismiss(animated: true)
+        }
+}
+
+// MARK: - Text field delegate
+extension NewPlaceViewController: UITextFieldDelegate {
+    // скрытие клав-ры при нажатии на done
+    func textFieldShouldReturn(_ textField: UITextField) -> Bool {
+        textField.resignFirstResponder()
+        return true
+    }
+    
+    @objc private func textFieldChanged() {
+        if placeName.text?.isEmpty == false {
+            saveButton.isEnabled = true
+        } else {
+            saveButton.isEnabled = false
+        }
+    }
+}
+
+// MARK: - Work with image
+extension NewPlaceViewController: UIImagePickerControllerDelegate, UINavigationControllerDelegate {
+    
+    func chooseImagePicker(source: UIImagePickerController.SourceType) {
+        if UIImagePickerController.isSourceTypeAvailable(source) {
+            
+            let imagePicker = UIImagePickerController()
+            imagePicker.delegate = self
+            imagePicker.allowsEditing = true
+            imagePicker.sourceType = source
+            present(imagePicker, animated: true)
+        }
+    }
+    
+    func imagePickerController(_ picker: UIImagePickerController,
+                               didFinishPickingMediaWithInfo info: [UIImagePickerController.InfoKey : Any]) {
+        
+        imageOfPlace.image = info[.editedImage] as? UIImage
+        imageOfPlace.contentMode = .scaleAspectFill
+        imageOfPlace.clipsToBounds = true
+        
+        isImageChanged = true
+        
+        dismiss(animated: true)
+    }
+}
